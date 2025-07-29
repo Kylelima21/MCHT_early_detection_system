3beacb5a82b6f3fe31053adbc9eca74d81d7875a
Kyle Lima <klima@schoodicinstitute.org>
2025-07-29 14:54:53 -0400

    first full draft of landing page

---
+++ email_alerts/email_sender.R
@@ -0,0 +1,60 @@
+## Script to produce and send the email alerts to those signed up
+## Schoodic Institute at Acadia National Park
+
+
+#------------------------------------------------#
+####           Packages Required              ####
+#------------------------------------------------#
+library(blastula)
+
+
+
+#------------------------------------------------#
+####         Create and send email            ####
+#------------------------------------------------#
+
+## Can run to see example of what the email will look like
+# my_email_object <- render_email('outputs/emailtest.Rmd')
+# print(my_email_object)
+
+
+## Create the credentials key needed to send from my email
+# create_smtp_creds_key(
+#   id = "kmail",
+#   user = "klima@schoodicinstitute.org",
+#   provider = "gmail",
+#   #overwrite = TRUE
+# )
+
+# create_smtp_creds_file(
+#   file = "kmail",
+#   user = "klima@schoodicinstitute.org",
+#   provider = "gmail"
+# )
+
+
+## Produce and send the email
+smtp_send(render_email('email_alerts/email_material.Rmd'), 
+          from = "klima@schoodicinstitute.org", 
+          to = c("klima@schoodicinstitute.org",
+                 "abe_miller-rushing@nps.gov",
+                 "nfisichelli@schoodicinstitute.org",
+                 "jesse_wheeler@nps.gov",
+                 "rebecca_cole-will@nps.gov",
+                 "bik_wheeler@nps.gov",
+                 "hwebber@schoodicinstitute.org",
+                 "sbenz@schoodicinstitute.org",
+                 "cnadeau@schoodicinstitute.org",
+                 "cschmitt@schoodicinstitute.org",
+                 "sobrien@schoodicinstitute.org",
+                 "ealbee@schoodicinstitute.org",
+                 "earlycareer@schoodicinstitute.org",
+                 "emma_lanning@nps.gov",
+                 "elanning@schoodicinstitute.org"),
+          subject = "Acadia National Park Citizen Science Report", 
+          credentials = creds_file("email_alerts/kmail")
+)
+
+
+
+
