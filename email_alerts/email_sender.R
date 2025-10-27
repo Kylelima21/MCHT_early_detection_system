36cd7be2a64b78d404cd48dd90db29e6d53a1c2d
Kyle Lima <97795211+Kylelima21@users.noreply.github.com>
2025-10-27 11:37:34 -0400

    Update email_sender.R

--- email_alerts/email_sender.R
+++ email_alerts/email_sender.R
@@ -25,7 +25,9 @@ smtp_send(render_email('email_alerts/email_material.Rmd'),
                  "kgentalen@mcht.org",
                  "emarshall@mcht.org",
                  "tbauer@mcht.org",
-                 "adevine@mcht.org"),
+                 "adevine@mcht.org",
+                 "mjenness@mcht.org",
+                 "jsattler@mcht.org"),
           subject = "Weekly early detection report", 
           credentials = creds_file("email_alerts/kmail")
 )
